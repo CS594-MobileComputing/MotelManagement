d7117ae9f9c0a1188b4a750a8df27d31e06eb707
Dhanashri Shinde <dhanashri17.shinde@gmail.com>
2016-06-10 07:38:59 -0700

    Charges xml and db

--- app/src/main/java/com/example/android/motelmgmtapp/activities/ChargesActivities.java
+++ app/src/main/java/com/example/android/motelmgmtapp/activities/ChargesActivities.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Switch;
@@ -16,15 +17,25 @@ import android.widget.Switch;
 import com.example.android.motelmgmtapp.DatabaseHandler;
 import com.example.android.motelmgmtapp.R;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
 public class ChargesActivities extends AppCompatActivity {
 
     public static final String ChargesData = "Charge" ;
     SharedPreferences sharedpreferences;
     DatabaseHandler dbh;
 
-    EditText total_cost,total_amount;
+    EditText total_cost ,total_amount, percentDiscount;
     boolean taxe;
-    float discount;
+    Switch s;
+
+    SeekBar seekBar;
+
+    Button calculate;
+    BigDecimal totalAmount, discountAmount, addTaxExmpt;
+    double discountPercent = 0.00;
+    double switchval = 0.15;
 
     Button b;
     @Override
@@ -38,12 +49,88 @@ public class ChargesActivities extends AppCompatActivity {
         dbh = new DatabaseHandler(this);
 
 
-        // total_cost,discount_percentage,tax_excempt,total_amount
-        final SeekBar seek = (SeekBar) findViewById(R.id.txtSeekbar);
-        final Switch s = (Switch)   findViewById(R.id.txtTaxE);
-        final String taxe = s.toString();
+        percentDiscount = (EditText) findViewById(R.id.discountValue);
         total_cost = (EditText) findViewById(R.id.txtTotal);
-        total_amount   = (EditText) findViewById(R.id.txtTotalAmount);
+        total_amount = (EditText) findViewById(R.id.txtTotalAmount);
+
+        seekBar = (SeekBar) findViewById(R.id.txtSeekbar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                percentDiscount.setText("%" + String.valueOf(i));
+                discountPercent = i*.01;
+                BigDecimal sbtl = new BigDecimal(Double.valueOf(total_cost.getText().toString()));
+                discountAmount = sbtl.multiply(new BigDecimal(discountPercent));
+                totalAmount = sbtl.subtract(discountAmount);
+                //String tipAmountText = NumberFormat.getCurrencyInstance().format(discountAmount);
+                //discountVal.setText(tipAmountText);
+                String totalAmountText =  NumberFormat.getCurrencyInstance().format(totalAmount);
+                total_amount.setText(totalAmountText);
+
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        s = (Switch)   findViewById(R.id.txtTaxE);
+        //final String taxe = s.toString();
+
+        //set the switch to OFF
+        s.setChecked(false);
+        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+
+                if(isChecked){
+                    taxe = false;
+
+                }else{
+                    taxe = true;
+                }
+
+            }
+        });
+
+
+        if(s.isChecked()){
+            taxe = false;
+        }
+        else {
+            taxe = true;
+        }
+
+        calculate = (Button) findViewById(R.id.calculate);
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (total_cost.getText() != null) {
+                    BigDecimal sbtl = new BigDecimal(Double.valueOf(total_cost.getText().toString()));
+                    discountAmount = sbtl.multiply(new BigDecimal(discountPercent));
+                    totalAmount = sbtl.subtract(discountAmount);
+                    if(taxe == true)
+                    {
+                        addTaxExmpt = totalAmount.multiply(new BigDecimal (switchval)).add(totalAmount);
+                        String totalAmountText =  NumberFormat.getCurrencyInstance().format(addTaxExmpt);
+                        //Log.d("Hi",totalAmountText );
+                        total_amount.setText(totalAmountText);
+                    }
+                    else{
+                        String totalAmountText =  NumberFormat.getCurrencyInstance().format(totalAmount);
+                        total_amount.setText(totalAmountText);
+                    }
+                }
+            }
+        });
 
         b = (Button) findViewById(R.id.payment);
 
@@ -65,8 +152,8 @@ public class ChargesActivities extends AppCompatActivity {
 
                                 editor.putString("total_cost" ,total_cost.getText().toString());
                                 editor.putString("total_amount" , total_amount.getText().toString());
-                                editor.putString("seek" , seek.toString());
-                                editor.putString("switch" , taxe);
+                                editor.putString("discount_percentage" , percentDiscount.getText().toString());
+                                editor.putString("switch" , taxe+"");
 
                                 editor.commit();
                                 Intent next = new Intent(ChargesActivities.this, PaymentActivities.class);
@@ -85,3 +172,4 @@ public class ChargesActivities extends AppCompatActivity {
     }
 
 }
+
